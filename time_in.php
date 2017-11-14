6e6c25dee855487c2aa06bc0c32f060b5764d980
Amirul Adli <sayaamiruladli@gmail.com>
2017-11-14 15:42:34 +0800

    Update time_in.php

--- time_in.php
+++ time_in.php
@@ -1,7 +1,17 @@
-<form action="timein_action.php" method="post" >
-<input type="text" id="serial_num" name="serial_num"  placeholder="Serial Number"/>
-<input type="text" id="user_ic" name="user_ic"  placeholder="IC Number"/>
-<input type="text" id="time_in" name="time_in"  placeholder="Time In"/>
+<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
 
+<form id="form" action="timein_action.php" method="post" >
+<input type="text" id="serial_num" name="serial_num"  placeholder="Serial Number"/>
 
 <input type="submit" name="submit" />
+  
+  <script>
+    
+    $('#serial_num).on('input', function(){
+      $('#form').submit();
+      });
+    
+  </script>
